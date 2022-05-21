1ad474996317327a352065882e85cf1e3a9725a3
Andrei <andreipetrisor1999@gmail.com>
2022-05-22 01:06:10 +0300

    refactor

--- src/main/java/com/project/Application.java
+++ src/main/java/com/project/Application.java
@@ -1,4 +1,4 @@
-package com.andrei.petrisor.food.delivery;
+package com.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/project/model/Company.java
+++ src/main/java/com/project/model/Company.java
@@ -1,4 +1,4 @@
-package com.andrei.petrisor.food.delivery.model;
+package com.project.model;
 
 import javax.persistence.Entity;
 import javax.persistence.Table;
