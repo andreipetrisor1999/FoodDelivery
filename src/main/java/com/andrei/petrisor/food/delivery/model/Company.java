dfbf88d3441ba0a30c15d38196645f4e625c6afa
Andrei <andreipetrisor1999@gmail.com>
2022-05-22 01:03:19 +0300

    init

---
+++ src/main/java/com/andrei/petrisor/food/delivery/model/Company.java
@@ -0,0 +1,10 @@
+package com.andrei.petrisor.food.delivery.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "company")
+public class Company {
+
+}
